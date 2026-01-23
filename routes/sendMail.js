3eaa975d6db4a7ad6f3e44ca97aabbccf6d0a08e
Akash-715 <akaskrs7190@gmail.com>
2026-01-23 22:44:42 +0530

    Made few changes in server.js

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -5,7 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (userEmail , name , message , phone) => {
 
-  await resend.emails.send({
+
+  const adminMail = {
     from:"SiliconVista <onboarding@resend.dev>",
     to:"siliconvista.org.enquiry@gmail.com",
     subject: "New Enquiry from Website",
@@ -16,20 +17,24 @@ const sendMail = async (userEmail , name , message , phone) => {
       <p><b>Message:</b> ${message}</p>
     `,
     reply_to: userEmail
-  });
+  }
 
-  //User email
-
-  await resend.emails.send({
+  const userMail = {
     from:"SiliconVista <onboarding@resend.dev>",
-    to:userEmail,
+    to: userEmail,
     subject: "Thanks for contacting us – We’ll get back to you shortly",
     html:`
       <p>Hi ${name},</p>
       <p>Thank you for contacting SiliconVista!</p>
       <p>We’ve received your enquiry and will get back to you shortly.</p>        
     `
-  });
+  }
+
+  await resend.emails.send(adminMail);
+
+  await new Promise(r => setTimeout(r,5000));
+
+  await resend.emails.send(userMail);
 
 };
 
