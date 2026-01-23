e55250dc38ed832a565f49b025a50fd8cbb41e7f
Akash-715 <akaskrs7190@gmail.com>
2026-01-23 20:16:25 +0530

    Changed the nodemailer to resend.

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,43 +1,37 @@
-import nodemailer from "nodemailer";
-import { text } from "express";
+import { Resend } from "resend";
 
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (userEmail , name , message , phone) => {
 
+  await resend.emails.send({
+    from:"SiliconVista <onboarding@resend.dev>",
+    to:"siliconvista.org.enquiry@gmail.com",
+    subject: "New Enquiry from Website",
+    html: `
+      <p><b>Name:</b> ${name}</p>
+      <p><b>Email:</b> ${userEmail}</p>
+      <p><b>Phone:</b> ${phone}</p>
+      <p><b>Message:</b> ${message}</p>
+    `,
+    reply_to: userEmail
+  });
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD
-  }
-});
+  //User email
 
+  await resend.emails.send({
+    from:"SiliconVista <onboarding@resend.dev>",
+    to:userEmail,
+    subject: "Thanks for contacting us – We’ll get back to you shortly",
+    html:`
+      <p>Hi ${name},</p>
+      <p>Thank you for contacting SiliconVista!</p>
+      <p>We’ve received your enquiry and will get back to you shortly.</p>        
+    `
+  });
 
-
-    const adminMail = {
-        from: process.env.MAIL_USERNAME,
-        to: "siliconvista.org.enquiry@gmail.com",
-        replyTo: userEmail, 
-        subject: "New Enquiry from Website",
-        html: `
-            <p><b>Name:</b> ${name}</p>
-            <p><b>Email:</b> ${userEmail}</p>
-            <p><b>Phone:</b> ${phone} </p>
-            <p><b>Message:</b> ${message}</p>
-        `
-    }
-
-    const userMail = {
-        from: process.env.MAIL_USERNAME,
-        to: userEmail,
-        subject: "Thanks for contacting us – We’ll get back to you shortly",
-        text: `We’ve received your enquiry`
-    };
-
-    await transporter.sendMail(adminMail);
-    await transporter.sendMail(userMail);
-}
+};
 
 
 export default sendMail;
